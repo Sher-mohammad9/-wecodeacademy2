8cab015f99273227034177573548311da8cdb506
sher-mohammad9 <hassan.deshwali0@gmail.com>
2022-12-13 16:21:51 +0530

    Modified js file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 "use strict"
-console.log("Hello")
+ function myMove() {
+            let id = null;
+            const elem = document.getElementById("animate");
+            let pos = 0;
+            clearInterval(id);
+            id = setInterval(frame, 2);
+            function frame() {
+              if (pos == 150) {
+                clearInterval(id);
+              } else {
+                pos++;
+                elem.style.top = pos + "px";
+                elem.style.left = pos + 200 + "px";
+              }
+            }
+          }
